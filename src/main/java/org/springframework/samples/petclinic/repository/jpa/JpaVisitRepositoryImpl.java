1629b3eaab69ab29b63b11b3b3f9ac113de868f0
Peter Murray <681306+peter-murray@users.noreply.github.com>
2021-01-20 13:00:30 +0000

    Adding injection problem

--- src/main/java/org/springframework/samples/petclinic/repository/jpa/JpaVisitRepositoryImpl.java
+++ src/main/java/org/springframework/samples/petclinic/repository/jpa/JpaVisitRepositoryImpl.java
@@ -56,8 +56,9 @@ public class JpaVisitRepositoryImpl implements VisitRepository {
     @Override
     @SuppressWarnings("unchecked")
     public List<Visit> findByPetId(Integer petId) {
-        Query query = this.em.createQuery("SELECT v FROM Visit v where v.pet.id= :id");
-        query.setParameter("id", petId);
+        Query query = this.em.createQuery("SELECT v FROM Visit v where v.pet.id=" + petId);
+        // Query query = this.em.createQuery("SELECT v FROM Visit v where v.pet.id= :id");
+        // query.setParameter("id", petId);
         return query.getResultList();
     }
 
